d68a92da777753f6824b720c29d47d12a9ae8766
LSW <LSW@narlabs.org.tw>
2025-04-24 14:27:01 +0800

    Add files via upload

---
+++ Mask_RCNN/wheel_level/readme_mrcnn_samples_wheel_code.py
@@ -0,0 +1,12 @@
+[2021-04-23] 補充access deep sensing論文中水位高度的第二種估算方法 mask rcnn water level
+複製一份flood_river--> tire --> wheel_water_level
+
+
+
+2020-07-27
+ipynb, py由正賢對大甲溪水域分割時修改balloon範例而成。
+
+後續先
+1.測試multi GPU模式訓練
+2.道路淹水偵測 增加mrcnn的水域分割
+3.腦腫瘤mri分割比賽
